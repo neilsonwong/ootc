ebcc3a9f4874d3802c294a8fe798d748b1921130
Clifton Chung <czbc00@gmail.com>
2019-09-21 22:07:32 -0400

    Made login, added material design.

--- ootc-ui/src/app/modules/authentication/authentication.module.ts
+++ ootc-ui/src/app/modules/authentication/authentication.module.ts
@@ -3,11 +3,21 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 
+// Material Imports
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field'; 
+import {MatInputModule} from '@angular/material/input'; 
+
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
-    AuthenticationRoutingModule
+    AuthenticationRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class AuthenticationModule { }
