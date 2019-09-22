d9ac1179feedb84cc67b8b05bf2a559796223b81
Martin Fan <m1fan@ryerson.ca>
2019-09-21 22:03:11 -0400

    Hi I have added a registration page

--- ootc-ui/src/app/modules/authentication/authentication.module.ts
+++ ootc-ui/src/app/modules/authentication/authentication.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [
+    LoginComponent, 
+    RegistrationComponent
+  ],
   imports: [
     CommonModule,
     AuthenticationRoutingModule
