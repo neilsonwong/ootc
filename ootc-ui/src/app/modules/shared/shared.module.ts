1e265957b21c5404c6f77a6a9f0a6c47244673ef
neilsonwong <hc3wong@engmail.uwaterloo.ca>
2019-09-23 17:57:58 -0400

    added footer bar component

--- ootc-ui/src/app/modules/shared/shared.module.ts
+++ ootc-ui/src/app/modules/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderBarComponent } from './components/header-bar/header-bar.component';
+import { FooterBarComponent } from './components/footer-bar/footer-bar.component';
 
 
 
 @NgModule({
-  declarations: [HeaderBarComponent],
+  declarations: [HeaderBarComponent, FooterBarComponent],
   imports: [
     CommonModule
   ]
