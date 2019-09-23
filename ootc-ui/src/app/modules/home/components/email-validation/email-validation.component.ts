f51993127793b577532c95faa004053dc0ab4d97
neilsonwong <hc3wong@engmail.uwaterloo.ca>
2019-09-23 18:03:09 -0400

    added other pages into home module

---
+++ ootc-ui/src/app/modules/home/components/email-validation/email-validation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-email-validation',
+  templateUrl: './email-validation.component.html',
+  styleUrls: ['./email-validation.component.scss']
+})
+export class EmailValidationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- ootc-ui/src/app/modules/home/home.module.ts
+++ ootc-ui/src/app/modules/home/home.module.ts
@@ -7,11 +7,15 @@ import { TopBarComponent } from './components/home/top-bar/top-bar.component';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AuthenticationModule } from '../authentication/authentication.module';
+import { LandingComponent } from './components/landing/landing.component';
+import { AboutComponent } from './components/about/about.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { EmailValidationComponent } from './components/email-validation/email-validation.component';
 
 @NgModule({
   declarations: [
     HomeComponent, 
-    TopBarComponent,
+    TopBarComponent, LandingComponent, AboutComponent, RegistrationComponent, EmailValidationComponent,
   ],
   imports: [
     CommonModule,
