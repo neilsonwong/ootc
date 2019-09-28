1ca029fa97ff8e24088bd8ca5a5f0a38628f73b9
Neilson Wong <hc3wong@engmail.uwaterloo.ca>
2019-09-28 18:00:28 -0400

    add app-container into project and make pages use it

---
+++ ootc-ui/src/app/modules/shared/components/app-container/app-container.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-container',
+  templateUrl: './app-container.component.html',
+  styleUrls: ['./app-container.component.scss']
+})
+export class AppContainerComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
