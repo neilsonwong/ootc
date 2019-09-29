0ebf2056448c260445094a28835b2c5ae4151e06
Martin Fan <m1fan@ryerson.ca>
2019-09-28 22:55:26 -0400

    working validation registration stuff

--- ootc-ui/src/app/app-routing.module.ts
+++ ootc-ui/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ErrorStateMatcher, ShowOnDirtyErrorStateMatcher } from '@angular/material/core';
 
 const routes: Routes = [
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [{
+    provide: ErrorStateMatcher, useClass:
+    ShowOnDirtyErrorStateMatcher
+  }]
 })
 export class AppRoutingModule { }
--- ootc-ui/src/app/modules/authentication/components/registration-form/registration-form.component.ts
+++ ootc-ui/src/app/modules/authentication/components/registration-form/registration-form.component.ts
@@ -1,29 +1,59 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
-import { Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl, FormControl, FormGroupDirective, NgForm } from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export function matchValidator(form: FormGroup){
+  const condition = form.get('password').value !== form.get('passwordConfirmation').value;
+  return condition ? { mismatch: true } : null;
+}
+
+export class CrossFieldErrorMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+      return control.touched && form.invalid;
+  }
+}
+
+export const formErrors: { [key: string]: string } = {
+  required: 'This is a required field',
+  pattern: 'Email must be a valid email address (leia@alderaan.net).',
+  minLength: 'Password must contain at least 8 characters.',
+  mismatch: 'Passwords don\'t match.',
+  unique: 'Passwords must contain at least 3 unique characters.'
+};
 
 @Component({
   selector: 'app-registration-form',
   templateUrl: './registration-form.component.html',
   styleUrls: ['./registration-form.component.scss']
 })
+
 export class RegistrationFormComponent implements OnInit {
   registrationForm: FormGroup;
-  angForm: FormGroup;
-  
+
   constructor(
+    public firstName = '',
+    public lastName = '',
+    public email = '',
+    public password = '',
+    public passwordConfirmation = '',
   ) { }
 
   ngOnInit() {
     // instantiate the form control
     this.registrationForm = new FormGroup({
-      firstName: new FormControl('',),
+      firstName: new FormControl('', [
+      ]),
       middleName: new FormControl(''),
       lastName: new FormControl(''),
       email: new FormControl('', [
         Validators.required,
+        Validators.email
       ]),
       password: new FormControl(''),
+      verifypassword: new FormControl('',[
+        Validators.required
+      ]),
       phoneNumber: new FormControl(''),
       age: new FormControl(''),
       experience: new FormControl(''),
@@ -32,9 +62,11 @@ export class RegistrationFormComponent implements OnInit {
   }
 
   confirmed = false;
+  confirmedpw = false;
 
   onRegister() {
     this.confirmed = true;
+    
     // do something here
   }
 }
