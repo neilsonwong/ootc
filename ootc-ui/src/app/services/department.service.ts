f3310f991e8c1bc1f9ca14d08bc999779684e6df
Neilson Wong <hc3wong@engmail.uwaterloo.ca>
2019-09-29 00:44:20 -0400

    update models and add TimeSlotView + ReservationView, mocked out service calls

--- ootc-ui/src/app/services/department.service.ts
+++ ootc-ui/src/app/services/department.service.ts
@@ -10,14 +10,21 @@ export class DepartmentService {
   constructor() { }
 
   getDepartments(): Observable<Department[]> {
-    return of(null);
+    return of([
+      new Department(1, 'Hospitality'),
+      new Department(2, 'Kitchen'),
+      new Department(3, 'Clothing Bank'),
+      new Department(4, 'Registration'),
+      new Department(5, 'Setup/Cleanup'),
+      new Department(6, 'Special Services'),
+    ]);
   }
 
   addDepartment(departmentName: string): Observable<Department> {
-    return of(null); 
+    return of(new Department(20, departmentName)); 
   }
 
   updateDepartment(department: Department): Observable<Department> {
-    return of(null); 
+    return of(department); 
   }
 }
