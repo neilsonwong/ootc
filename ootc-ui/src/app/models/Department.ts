94b8cab6beec56fc9fcc540b2cc52815c689c5c5
Clifton Chung <czbc00@gmail.com>
2019-09-28 22:08:08 -0400

    Added some TS MODELS

---
+++ ootc-ui/src/app/models/Department.ts
@@ -0,0 +1,13 @@
+export class Department {
+    id: number;
+    name: string;
+
+
+    constructor ( 
+        id: number, 
+        name: string 
+        ){
+        this.id = id,
+        this.name = name
+    }
+}
---
+++ ootc-ui/src/app/models/Reservation.ts
@@ -0,0 +1,18 @@
+export class Reservation {
+    id: number;
+    user: string;
+    timeSlot: string;
+    isAttended: boolean;
+
+    constructor (
+        id: number,
+        user: string,
+        timeSlot: string,
+        isAttended: boolean
+    ){
+        this.id = id,
+        this.user = user,
+        this.timeSlot = timeSlot,
+        this.isAttended = isAttended
+    }
+}
