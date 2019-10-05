47b2a84493222eea4821f9188c552ae243f82d16
Martin Fan <m1fan@ryerson.ca>
2019-10-05 16:36:24 -0400

    added buttons for department spotlight

--- ootc-ui/src/app/modules/reservation/components/department-spotlight/department-spotlight.component.ts
+++ ootc-ui/src/app/modules/reservation/components/department-spotlight/department-spotlight.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl, FormControl, FormGroupDirective, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-department-spotlight',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./department-spotlight.component.scss']
 })
 export class DepartmentSpotlightComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  select = false;
+  onSelect(){
+    this.select = true;
   }
 
 }
--- ootc-ui/src/app/modules/reservation/components/reservation-details/reservation-details.component.ts
+++ ootc-ui/src/app/modules/reservation/components/reservation-details/reservation-details.component.ts
@@ -5,10 +5,14 @@ import { Component, OnInit, Input } from '@angular/core';
   templateUrl: './reservation-details.component.html',
   styleUrls: ['./reservation-details.component.scss']
 })
+
+
 export class ReservationDetailsComponent implements OnInit {
   @Input() reservation: any;
+
   constructor() { }
 
+  
   ngOnInit() {
   }
 
