6f1563cfe7cbbe68137859279f5abf74995385b1
Neilson Wong <hc3wong@engmail.uwaterloo.ca>
2019-10-13 01:13:00 -0400

    added config checker

--- server/index.js
+++ server/index.js
@@ -3,11 +3,12 @@
 const db = require('./src/db/db');
 const restapi = require('./src/restapi');
 const accountManager = require('./src/core/accountManager');
+const configChecker = require('./src/util/configChecker');
 
 async function main() {
+    configChecker.checkConfig();
+    
     await db.init();
-    // await dbTests.test();
-
     await accountManager.setupDefaultUsers();
     restapi.init();
 }
---
+++ server/src/util/configChecker.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const configEg = require('../../config.eg');
+const config = require('../../config');
+const logger = require('../logger');
+
+function checkConfig() {
+    const key = compareKeys(configEg, config)
+    if (key !== null) {
+        logger.error('configs don\'t match, please make sure config.json is compatible with config.eg.json');
+        logger.error(`the violating key is '${key}'`);
+    }
+    else {
+        logger.info('config.json is VALID');
+    }
+}
+
+function compareKeys(a, b) {
+    for (let key of Object.keys(a)) {
+        if (b[key] === undefined) {
+            return key;
+        }
+        // this bottom part might not be perfect, but don't need it now
+        else if (typeof a[key] === 'object') {
+            if (compareKeys(a[key], b[key]) !== null) {
+                return key;
+            }
+        }
+    }
+    return null;
+}
+
+module.exports = {
+    checkConfig: checkConfig
+};
