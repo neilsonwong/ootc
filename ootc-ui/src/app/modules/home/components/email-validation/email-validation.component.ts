e1c92aba3d2e6cdf4b58af548fc187b018181f7b
Neilson Wong <hc3wong@engmail.uwaterloo.ca>
2019-10-17 18:41:12 -0400

    add email validation component

--- ootc-ui/src/app/modules/home/components/email-validation/email-validation.component.ts
+++ ootc-ui/src/app/modules/home/components/email-validation/email-validation.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-email-validation',
@@ -6,10 +10,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./email-validation.component.scss']
 })
 export class EmailValidationComponent implements OnInit {
+  public email: string;
+  public validationCodeString: string;
+  public isValidating: boolean = true;
+  public validationSuccess: boolean;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute,
+    private authService: AuthenticationService) { }
 
   ngOnInit() {
+    const combinedCode: string = this.activatedRoute.snapshot.queryParamMap.get('code');
+    // decode the combined code
+    const combinedString: string = window.atob(combinedCode);
+    [this.email, this.validationCodeString] = combinedString.split(':');
+    this.validate();
   }
 
+  validate() {
+    try {
+      const validationCode = parseInt(this.validationCodeString);
+      console.log(this.email)
+      console.log(validationCode)
+      this.authService.validateEmail(this.email, validationCode)
+        .subscribe((result: boolean) => {
+          this.validationSuccess = result;
+          this.isValidating = false;
+        });
+    }
+    catch(e) {
+      this.validationSuccess = false;
+      this.isValidating = false;
+      console.log('there was an error validating the email');
+      console.log(e);
+    }
+  }
+
+  resendValidation() {
+    alert('resend validation');
+  }
 }
