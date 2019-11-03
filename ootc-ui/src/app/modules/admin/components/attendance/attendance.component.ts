3990acbae7318f4d1b914478ddd06ac1b605505d
Clifton Chung <czbc00@gmail.com>
2019-11-03 17:44:09 -0500

    Created attendance component.

--- ootc-ui/src/app/modules/admin/components/attendance/attendance.component.ts
+++ ootc-ui/src/app/modules/admin/components/attendance/attendance.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ReservationService } from 'src/app/services/reservation.service';
+import { User } from 'src/app/models/User';
 
 @Component({
   selector: 'app-attendance',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AttendanceComponent implements OnInit {
 
-  constructor() { }
+  private users: User[];
+  private date: Date = new Date();
+  
+  public userIdentifyForm: FormGroup;
+  public userFound: boolean = false;
+  public confirmed = false;
+
+  constructor(private fb: FormBuilder,
+    private reservationService: ReservationService) { }
 
   ngOnInit() {
+    this.userIdentifyForm = this.fb.group({
+      email: ['', [
+        Validators.pattern('[\\w\\.-]+@[\\w\\.-]+\\.\\w{2,4}'),
+    ]]
+    });
   }
+
+  userCheckIn() {
+    this.reservationService.reservationSignin(this.userIdentifyForm.get('email').value).subscribe();
+    this.confirmed = true;
+    console.log("it works");
+  }  
 
 }
